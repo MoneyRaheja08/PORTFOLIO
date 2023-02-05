71e049c48c00cb252a1328150c9bff4eab4e28d9
MoneyRaheja08 <122489723+MoneyRaheja08@users.noreply.github.com>
2023-02-05 16:28:38 -0500

    add routes in index.js

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,23 @@ router.get('/', function(req, res, next) {
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Home' });
+});
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Home' });
+});
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Home' });
+});
+/* GET Contacts page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Home' });
+});
+
 
 
 
